a809388413a9a252af75dd2c4b035bc590db5dbc
Timur Agliev <timur@agliev.ru>
2018-11-26 00:54:20 +0400

    check

--- Project Journal/AddViewController.swift
+++ Project Journal/AddViewController.swift
@@ -53,44 +53,44 @@ class AddViewController: UIViewController, UITextFieldDelegate {
         var nameCount = nameToCount.count
         var secondNameCount = secondNameToCount.count
 
-    guard let nameEmpty = nameTextField.text, let secondNameEmpty = secondNameTextField.text, let markEmpty = markTextFiled.text else {return}
-        guard nameEmpty.isEmpty == false && secondNameEmpty.isEmpty == false && markEmpty.isEmpty == false else {return present(alertForAll, animated: true, completion: nil) }
-        
-        //Проверка написания имени
-        while nameCount != 0 {
-            guard let nameToCount = nameTextField.text else { return}
-            for one in nameToCount{
-                let ruCharacters = "йцукенгшщзхъфывапролджэёячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ"
-                let engCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
-                guard ruCharacters.contains(one) || engCharacters.contains(one) else {return present(doubleAlert, animated: true, completion: nil)}
-                nameCount -= 1
-            }
-        }
-        //Проверка написания фамилии
-        while secondNameCount != 0 {
-            guard let secondNameToCount = secondNameTextField.text else {return}
-            for one in secondNameToCount{
-                let ruCharacters = "йцукенгшщзхъфывапролджэёячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ"
-                let engCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
-                guard ruCharacters.contains(one) || engCharacters.contains(one) else {return present(doubleAlert, animated: true, completion: nil)}
-                secondNameCount -= 1
-            }
-        }
-        //Проверка написания оценки
-        if let mark = UInt(markTextFiled.text!) {
-            var markRange = 5
-            while markRange != 0 {
-                if mark == markRange {
-                    performSegue(withIdentifier: "BackSave", sender: nil)
-                    break
-                } else {
-                    markRange -= 1
-                }
-            }
-            guard markRange != 0 else {return present(markAttention, animated: true, completion: nil)}
-        } else {
-            present(markAttention, animated: true, completion: nil)
-        }
+//    guard let nameEmpty = nameTextField.text, let secondNameEmpty = secondNameTextField.text, let markEmpty = markTextFiled.text else {return}
+//        guard nameEmpty.isEmpty == false && secondNameEmpty.isEmpty == false && markEmpty.isEmpty == false else {return present(alertForAll, animated: true, completion: nil) }
+//        
+//        //Проверка написания имени
+//        while nameCount != 0 {
+//            guard let nameToCount = nameTextField.text else { return}
+//            for one in nameToCount{
+//                let ruCharacters = "йцукенгшщзхъфывапролджэёячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ"
+//                let engCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+//                guard ruCharacters.contains(one) || engCharacters.contains(one) else {return present(doubleAlert, animated: true, completion: nil)}
+//                nameCount -= 1
+//            }
+//        }
+//        //Проверка написания фамилии
+//        while secondNameCount != 0 {
+//            guard let secondNameToCount = secondNameTextField.text else {return}
+//            for one in secondNameToCount{
+//                let ruCharacters = "йцукенгшщзхъфывапролджэёячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ"
+//                let engCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+//                guard ruCharacters.contains(one) || engCharacters.contains(one) else {return present(doubleAlert, animated: true, completion: nil)}
+//                secondNameCount -= 1
+//            }
+//        }
+//        //Проверка написания оценки
+//        if let mark = UInt(markTextFiled.text!) {
+//            var markRange = 5
+//            while markRange != 0 {
+//                if mark == markRange {
+//                    performSegue(withIdentifier: "BackSave", sender: nil)
+//                    break
+//                } else {
+//                    markRange -= 1
+//                }
+//            }
+//            guard markRange != 0 else {return present(markAttention, animated: true, completion: nil)}
+//        } else {
+//            present(markAttention, animated: true, completion: nil)
+//        }
 }
     // Кнопка Отменить
     @IBAction func cancelButton(_ sender: UIButton) {
